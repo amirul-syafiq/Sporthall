5313d648ec61a05fee10c2ff5e2b493303231b60
amirul-syafiq <amirulsyafiq@graduate.utm.my>
2022-07-24 12:26:19 +0800

    prep

--- api/index.php
+++ api/index.php
@@ -4,7 +4,14 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
-require 'db.php';
+require '../config/db.php';
 $app = new \Slim\App;
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("This is root page,");
+
+    return $response;
+});
+
 
 $app->run();
